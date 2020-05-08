0745edbd1f77877739963d547c2545cc026e3acb
Brian Russell <bruss@brians-mbp.lan>
2020-05-08 12:02:36 -0400

    Successfully rendered API data to browser. Built basic structure of useForm.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,9 @@
 // write your custom hook here to control your checkout form
+
+import {useState} from 'react'
+
+const useForm = (key, initalValue) =>{
+    const [values, SetValues] = useState(key, initalValue)
+}
+
+export default useForm
